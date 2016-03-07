81bed79b0c3804542b929f150d6a10001abfa15c
mshockwave <yihshyng223@gmail.com>
2016-03-07 20:20:48 +0800

    Update 2016-03-07T12:20:35.496Z

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var path = require('path');
+var merge = require('merge-stream');
+var runSequence = require('run-sequence');
+
+var DIST = 'dist';
+
+var dist = function(subpath) {
+    return !subpath ? DIST : path.join(DIST, subpath);
+};
+
+gulp.task('vulcanize', function(){
+    return gulp.src('elements/elements.html')
+        .pipe($.vulcanize({
+            stripComments: true,
+            inlineCss: true,
+            inlineScripts: true
+        }))
+        .pipe(gulp.dest(dist('elements')));
+});
+
+gulp.task('copyImg', function(){
+    return gulp.src([
+        './img/**/*'
+    ], {
+        dot: true
+    }).pipe(gulp.dest(dist('img')));
+});
+gulp.task('copy', function() {
+    var app = gulp.src([
+        './*',
+        '!test',
+        '!elements',
+        '!bower_components',
+        '!node_modules',
+        '!dist',
+        '!cache-config.json',
+        '!**/.DS_Store',
+        '!**/.idea'
+    ], {
+        dot: true
+    }).pipe(gulp.dest(dist()));
+
+    // Copy over only the bower_components we need
+    // These are things which cannot be vulcanized
+    var bower = gulp.src([
+        'bower_components/' +
+        '{webcomponentsjs,platinum-sw,sw-toolbox,promise-polyfill,dentistjs,jquery,jquery.scrollTo}/**/*'
+    ]).pipe(gulp.dest(dist('bower_components')));
+
+    return merge(app, bower);
+});
+
+gulp.task('build', function(callback){
+    runSequence(
+        'vulcanize',
+        'copy',
+        'copyImg',
+        callback
+    );
+});
+
+gulp.task('deploy', function(){
+    return gulp.src(dist('**/*'))
+        .pipe($.ghPages({
+            branch: "release"
+        }));
+});
+
+gulp.task('build-deploy', function(callback){
+    runSequence(
+        'build',
+        'deploy',
+        callback
+    );
+});
